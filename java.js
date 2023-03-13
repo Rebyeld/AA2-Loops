07edb17c640680f6041b1a720b8e7e45de4b58ed
Rebyeld <127067605+Rebyeld@users.noreply.github.com>
2023-03-13 11:01:28 +0800

    Add files via upload

---
+++ java.js
@@ -0,0 +1,20 @@
+var x = 2;
+while (x < 69) {
+    document.writeln (x);
+    x +=2;
+}
+
+var y = 3;
+do {
+    document.writeln(y);
+    y +=3;
+}
+while (y < 61);
+
+var z;
+
+for (z = 5; z < 51; z +=5){
+    document.writeln(z);
+
+} 
+
